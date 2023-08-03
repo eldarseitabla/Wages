40ee7faa7d77c1fe168b6f0e30f42236068786d5
Eldar Sieitablaiev <eldar.seitabla@outlook.com>
2023-08-03 12:32:57 +0100

    Add all buttons

--- Wages/Wages.Designer.cs
+++ Wages/Wages.Designer.cs
@@ -29,8 +29,16 @@
         private void InitializeComponent()
         {
             noOfHoursWorkedLabel = new Label();
-            numericUpDown1 = new NumericUpDown();
-            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            noOfHoursWorkedNumericUpDown = new NumericUpDown();
+            selectHoursNumericUpDown = new NumericUpDown();
+            selectHoursLabel = new Label();
+            overtimeCheckBox = new CheckBox();
+            selectHourlyRateLabel = new Label();
+            selectHourlyRateComboBox = new ComboBox();
+            calculateButton = new Button();
+            clearButton = new Button();
+            ((System.ComponentModel.ISupportInitialize)noOfHoursWorkedNumericUpDown).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)selectHoursNumericUpDown).BeginInit();
             SuspendLayout();
             // 
             // noOfHoursWorkedLabel
@@ -43,23 +51,100 @@
             noOfHoursWorkedLabel.Text = "No. Of Hours Worked";
             noOfHoursWorkedLabel.Click += noOfHoursWorkedLabel_Click;
             // 
-            // numericUpDown1
+            // noOfHoursWorkedNumericUpDown
             // 
-            numericUpDown1.Location = new Point(416, 95);
-            numericUpDown1.Name = "numericUpDown1";
-            numericUpDown1.Size = new Size(116, 31);
-            numericUpDown1.TabIndex = 1;
+            noOfHoursWorkedNumericUpDown.Location = new Point(386, 99);
+            noOfHoursWorkedNumericUpDown.Name = "noOfHoursWorkedNumericUpDown";
+            noOfHoursWorkedNumericUpDown.Size = new Size(131, 31);
+            noOfHoursWorkedNumericUpDown.TabIndex = 1;
+            noOfHoursWorkedNumericUpDown.ValueChanged += noOfHoursWorkedNumericUpDown_ValueChanged;
+            // 
+            // selectHoursNumericUpDown
+            // 
+            selectHoursNumericUpDown.Location = new Point(425, 163);
+            selectHoursNumericUpDown.Name = "selectHoursNumericUpDown";
+            selectHoursNumericUpDown.Size = new Size(92, 31);
+            selectHoursNumericUpDown.TabIndex = 3;
+            selectHoursNumericUpDown.ValueChanged += selectHoursNumericUpDown_ValueChanged;
+            // 
+            // selectHoursLabel
+            // 
+            selectHoursLabel.AutoSize = true;
+            selectHoursLabel.Location = new Point(308, 166);
+            selectHoursLabel.Name = "selectHoursLabel";
+            selectHoursLabel.Size = new Size(111, 25);
+            selectHoursLabel.TabIndex = 4;
+            selectHoursLabel.Text = "Select Hours";
+            selectHoursLabel.Click += selectHoursLabel_Click;
+            // 
+            // overtimeCheckBox
+            // 
+            overtimeCheckBox.AutoSize = true;
+            overtimeCheckBox.Location = new Point(176, 165);
+            overtimeCheckBox.Name = "overtimeCheckBox";
+            overtimeCheckBox.Size = new Size(111, 29);
+            overtimeCheckBox.TabIndex = 5;
+            overtimeCheckBox.Text = "Overtime";
+            overtimeCheckBox.UseVisualStyleBackColor = true;
+            overtimeCheckBox.CheckedChanged += overtimeCheckBox_CheckedChanged;
+            // 
+            // selectHourlyRateLabel
+            // 
+            selectHourlyRateLabel.AutoSize = true;
+            selectHourlyRateLabel.Location = new Point(176, 281);
+            selectHourlyRateLabel.Name = "selectHourlyRateLabel";
+            selectHourlyRateLabel.Size = new Size(156, 25);
+            selectHourlyRateLabel.TabIndex = 6;
+            selectHourlyRateLabel.Text = "Select Hourly Rate";
+            selectHourlyRateLabel.Click += selectHourlyRateLabel_Click;
+            // 
+            // selectHourlyRateComboBox
+            // 
+            selectHourlyRateComboBox.FormattingEnabled = true;
+            selectHourlyRateComboBox.Location = new Point(344, 281);
+            selectHourlyRateComboBox.Name = "selectHourlyRateComboBox";
+            selectHourlyRateComboBox.Size = new Size(158, 33);
+            selectHourlyRateComboBox.TabIndex = 7;
+            selectHourlyRateComboBox.SelectedIndexChanged += selectHourlyRateComboBox_SelectedIndexChanged;
+            // 
+            // calculateButton
+            // 
+            calculateButton.Location = new Point(176, 458);
+            calculateButton.Name = "calculateButton";
+            calculateButton.Size = new Size(112, 34);
+            calculateButton.TabIndex = 8;
+            calculateButton.Text = "Calculate";
+            calculateButton.UseVisualStyleBackColor = true;
+            calculateButton.Click += calculateButton_Click;
+            // 
+            // clearButton
+            // 
+            clearButton.Location = new Point(405, 458);
+            clearButton.Name = "clearButton";
+            clearButton.Size = new Size(112, 34);
+            clearButton.TabIndex = 9;
+            clearButton.Text = "Clear";
+            clearButton.UseVisualStyleBackColor = true;
             // 
             // Wages
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(numericUpDown1);
+            ClientSize = new Size(686, 603);
+            Controls.Add(clearButton);
+            Controls.Add(calculateButton);
+            Controls.Add(selectHourlyRateComboBox);
+            Controls.Add(selectHourlyRateLabel);
+            Controls.Add(overtimeCheckBox);
+            Controls.Add(selectHoursLabel);
+            Controls.Add(selectHoursNumericUpDown);
+            Controls.Add(noOfHoursWorkedNumericUpDown);
             Controls.Add(noOfHoursWorkedLabel);
             Name = "Wages";
             Text = "Wapes";
-            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            Load += Wages_Load;
+            ((System.ComponentModel.ISupportInitialize)noOfHoursWorkedNumericUpDown).EndInit();
+            ((System.ComponentModel.ISupportInitialize)selectHoursNumericUpDown).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -67,6 +152,13 @@
         #endregion
 
         private Label noOfHoursWorkedLabel;
-        private NumericUpDown numericUpDown1;
+        private NumericUpDown noOfHoursWorkedNumericUpDown;
+        private NumericUpDown selectHoursNumericUpDown;
+        private Label selectHoursLabel;
+        private CheckBox overtimeCheckBox;
+        private Label selectHourlyRateLabel;
+        private ComboBox selectHourlyRateComboBox;
+        private Button calculateButton;
+        private Button clearButton;
     }
 }
--- Wages/Wages.cs
+++ Wages/Wages.cs
@@ -11,5 +11,45 @@ namespace Wages
         {
 
         }
+
+        private void noOfHoursWorkedNumericUpDown_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void overtimeCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Wages_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void selectHoursLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void selectHoursNumericUpDown_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void selectHourlyRateLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void selectHourlyRateComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
